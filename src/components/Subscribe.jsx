0880f8f70356d657ecbc1b40fc87423dc23f6ba5
Eleonora Alminson <ellialminson@me.com>
2025-02-28 10:33:59 +0100

    Update Subscribe component text for conciseness

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -9,7 +9,7 @@ const Subscribe = () => {
           <img src="images/icons/notification-bell.svg" alt="Notification Bell" />
           <h2>
             Subscribe to our newsletter
-            <span>to stay informed about our latest updates</span>
+            <span>to stay informed about latest updates</span>
           </h2>
         </div>
 
